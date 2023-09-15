89e194a35eca468033de35f7de74ea35b6f8f756
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-09-16 03:18:29 +0700

    update task competence

--- 09_Code-Competence-KM1/Project/script/index.js
+++ 09_Code-Competence-KM1/Project/script/index.js
@@ -1,1 +1,8 @@
-const tes = "";
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    navbar.classList.add("shadow");
+  } else {
+    navbar.classList.remove("shadow");
+  }
+});
