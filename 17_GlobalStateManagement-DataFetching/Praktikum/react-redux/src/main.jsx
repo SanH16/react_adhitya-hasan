22067061cc9ebd29f57d167ab2fb5d9a485a12bc
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-28 04:36:20 +0700

    just add persist gate in main.jsx

--- 17_GlobalStateManagement-DataFetching/Praktikum/react-redux/src/main.jsx
+++ 17_GlobalStateManagement-DataFetching/Praktikum/react-redux/src/main.jsx
@@ -9,13 +9,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import { Provider } from "react-redux";
-import store from "./store/index.js";
+import store, { persistor } from "./store/index.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
