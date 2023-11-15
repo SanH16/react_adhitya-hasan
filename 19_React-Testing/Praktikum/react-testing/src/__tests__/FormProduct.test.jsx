0f9229227966a911809cb46912300f4c3e6634a3
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-16 01:29:49 +0700

    update task react testing

--- 19_React-Testing/Praktikum/react-testing/src/__tests__/FormProduct.test.jsx
+++ 19_React-Testing/Praktikum/react-testing/src/__tests__/FormProduct.test.jsx
@@ -1,9 +1,110 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import FormProduct from "../components/create-product/FormProduct";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../store";
 
 describe("FormProduct", () => {
-  it("renders without crashing correctly", () => {
-    const { container } = render(<FormProduct />);
+  it("should render form product correctly", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
     expect(container).toBeInTheDocument();
+  });
+
+  it("should render input product name is correctly", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const inputs = screen.getAllByRole("textbox");
+    expect(inputs.length).toEqual(2);
+  });
+
+  it("product name input should work correctly", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const productName = screen.getByLabelText(/product name/i);
+    fireEvent.change(productName, { target: { value: "Nike air Jordan" } });
+    expect(productName).toHaveValue("Nike air Jordan");
+  });
+
+  it("should render input field and work correctly", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const inputs = screen.getAllByRole("textbox");
+    expect(inputs.length).toEqual(2);
+
+    const productCategory = screen.getByRole("combobox");
+    expect(productCategory).toBeInTheDocument();
+
+    const flexradio = screen.getAllByRole("radio");
+    expect(flexradio.length).toEqual(4);
+
+    const textArea = screen.getByLabelText(/additional description/i);
+    fireEvent.change(textArea, { target: { value: "Brand new in Box" } });
+    expect(textArea).toHaveValue("Brand new in Box");
+
+    const productPrice = screen.getByLabelText(/product price/i);
+    fireEvent.change(productPrice, { target: { value: 100 } });
+    expect(productPrice).toHaveValue(100);
+
+    const submitButton = screen.getByRole("button", { name: /submit/i });
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it("product name input must beetwen 10 and 25 characters, only using letters, numbers, underscore, and hyphen", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const productName = screen.getByLabelText(/product name/i);
+    fireEvent.change(productName, { target: { value: "product name test" } });
+    const regex = /^[a-zA-Z0-9_-]{10,25}$/ && /[!@#$%^&*()_+//\\{}]/;
+    const inputProductName = productName.value;
+
+    const validation = regex.test(inputProductName);
+    expect(validation).toBe(false);
+  });
+
+  it("product name input must beetwen 10 and 25 characters", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <FormProduct />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    const productName = screen.getByLabelText(/product name/i);
+    fireEvent.change(productName, { target: { value: "Dua puluh empat karakter" } });
+    const inputProductName = productName.value;
+
+    expect(inputProductName.length).not.toBeGreaterThan(25);
   });
 });
--- 19_React-Testing/readme.md
+++ 19_React-Testing/readme.md
@@ -2,4 +2,69 @@
 
 ## 3 Poin penting
 
-## React Testing
+### React Testing
+
+React testing adalah proses pengujian aplikasi React untuk memastikan bahwa aplikasi tersebut berfungsi dengan benar. Testing adalah bagian penting dari pengembangan perangkat lunak karena dapat membantu menemukan dan memperbaiki bug, meningkatkan kualitas aplikasi, dan memastikan bahwa aplikasi berfungsi dengan baik di semua kondisi.
+
+### Unit Testing
+
+Unit testing adalah jenis testing yang paling dasar dalam React testing. Unit testing dilakukan untuk menguji fungsionalitas komponen React secara individu. Unit testing biasanya dilakukan dengan menggunakan library seperti Jest.
+
+### Jenis react testing
+
+Ada berbagai jenis react testing selain Unit Testing, antara lain:
+
+- Integration testing: Integration testing adalah jenis testing yang dilakukan untuk menguji interaksi antar komponen React.
+- End-to-end testing: End-to-end testing adalah jenis testing yang dilakukan untuk menguji keseluruhan aplikasi React.
+
+<hr />
+
+## 📝 Task
+
+### Soal Prioritas 1 (Nilai 80)
+
+- Buatlah file baru bernama CreateProduct.test.js didirectory yang sama tempat CreateProduct.jsx disimpan
+
+<p align="center">
+    <img src="Screenshots/create-file.png" alt="gambar" />
+</p>
+
+- Pada file CreateProduct.test.js buatlah test untuk memastikan bahwa form input Product Name dapat menerima input teks dan menampilkannya di halaman.
+
+<p align="center">
+    <img src="Screenshots/render-product-name.png" alt="gambar" />
+</p>
+
+- Buatlah test untuk memastikan bahwa pilihan setiap form yang dipilih dapat disimpan dan ditampilkan dengan benar.
+
+<p align="center">
+    <img src="Screenshots/render-all-fields.png" alt="gambar" />
+</p>
+
+### Soal Prioritas 2 (Nilai 20)
+
+- Buatlah test untuk memastikan validasi form input yang benar, seperti memastikan bahwa Product Name tidak boleh kosong, tidak mengandung karakter @/#{}.
+
+<p align="center">
+    <img src="Screenshots/product-name-contain.png" alt="gambar" />
+</p>
+
+<p align="center">
+    <img src="Screenshots/product-name-regex.png" alt="gambar" />
+</p>
+
+- Buatlah test untuk memastikan validasi form input yang benar bahwaProduct Name tidak melebihin 25 karakter
+
+<p align="center">
+    <img src="Screenshots/product-name-more25char.png" alt="gambar" />
+</p>
+
+-Buatlah test untuk memastikan validasi semua form field tersebut tidak boleh kosong.
+
+<p align="center">
+    <img src="Screenshots/render-all-fields.png" alt="gambar" />
+</p>
+
+<p align="center">
+    <img src="Screenshots/result-test.png" alt="gambar" />
+</p>
