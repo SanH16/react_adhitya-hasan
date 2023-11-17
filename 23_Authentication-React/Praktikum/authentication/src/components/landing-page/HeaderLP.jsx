7478e5564bf00e25f32f5312169f8528d85f50dd
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-11-17 21:47:15 +0700

    update pages components and other

---
+++ 23_Authentication-React/Praktikum/authentication/src/components/landing-page/HeaderLP.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function HeaderLP() {
+  return (
+    <>
+      <section className="container">
+        <nav className="fixed-top navbar navbar-expand-lg shadow-sm bg-body-tertiary pt-3 px-4">
+          <div className="container-fluid">
+            <Link to="/">
+              <h4 className="navbar-brand">Landing Page</h4>
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item pe-4">
+                  <Link to="/">
+                    <span
+                      className="nav-link active text-bg-primary rounded d-inline-flex px-2 mb-2"
+                      aria-current="page"
+                    >
+                      Home
+                    </span>
+                  </Link>
+                </li>
+                <li className="nav-item pe-4">
+                  <Link to="/create-product">
+                    <span className="nav-link anav rounded d-inline-flex px-2 mb-2">Create Product</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+}
